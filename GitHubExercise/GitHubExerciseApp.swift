ce45c4a3f77fb223bc352eba318a4e83fa4ea72a
Christopher Kennedy <kennednu@bc.edu>
2023-01-23 14:29:54 -0500

    Initial Commit

---
+++ GitHubExercise/GitHubExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExerciseApp.swift
+//  GitHubExercise
+//
+//  Created by Christopher Kennedy on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
